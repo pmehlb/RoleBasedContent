b3e22b842ebdd321a03f9863ba40136c88a55c07
Patrick Mehlbaum <45018548+pmehlb@users.noreply.github.com>
2020-11-11 09:36:10 -0500

    Update rbc.js

--- rbc.js
+++ rbc.js
@@ -1,8 +1,6 @@
 $('p br:only-child').each(function(i, el) {
-    if ($(el).parent().html() === "<br>\n") { $(this).parent().remove(); }
-});
-$('p br:only-child').each(function(i, el) {
-    if ($(this).parent().html().includes("<br>\n<")) { $(this).remove(); } 
+	if ($(el).parent().html() === "<br>\n") { $(this).parent().remove(); }
+	if ($(this).parent().html().includes("<br>\n<")) { $(this).remove(); } 
 });
 
 /* still need to test this js - i wrote this whole plugin at like...2 in the morning
